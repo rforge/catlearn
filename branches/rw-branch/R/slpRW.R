7311f84a16773af36b12ba431128b50daa150f0b
ajwills <ajwills@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-02-01 11:06:15 +0000

    Tidied and increased efficiency of slpRW.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/rw-branch/R/slpRW.R
+++ branches/rw-branch/R/slpRW.R
@@ -1,63 +1,28 @@
-#slpRW
-
-#st and tr as inputs into slpRW function.
-#Also includes option for extended output - set to TRUE if required,
-#otherwise it defaults to false:
 slpRW <- function(st, tr, xtdo = FALSE) {
-
-  #Need something for the outputs to go into (n.b. xout refers
-  #to extended output):
-  out <- NULL
-  xout <- NULL
-  
-  #Need to unpack what is contained within st list:
-  lr.m <- st$lr # learning rate
-  w.m <- st$w #vector of stimuli weights
-  colskip.m <- st$colskip #columns to skip before stimuli columns
-  
-  #Runs loop for however many rows are contained within input matrix:
-  for(i in 1:nrow(tr)) {
-    #Data is pulled out of each row in turn:
-    arow <- tr[i, ]
-    
-    #Designates number of columns (i.e. length of vector) containing 
-    #weights of stimuli:
-    nw <- length(w.m)
-    
-    #Model needs to reset initial wieghts each time there is a change
-    #in participant; as inticated in 'ctrl' column of input matrix:
-    if (arow['ctrl'] == 1) {
-      w.m <- st$w 
-      }
-    
-    #Teacher on/off acts as lambda within this model i.e. 1 or 0:
-    lambda <- arow['t'] 
-    
-    #This creates a vector of activations by reading the contents of the
-    #stimuli colums within input matrix:
-    a <- arow[(colskip.m + 1):(colskip.m + nw)] 
-  
-    #This is the RW equation - where delta is change in assoc strength:
-    suma <- sum(a*w.m)
-  
-    delta <- lr.m * (lambda - suma)
-    
-    #New weights are weights plus delta; unless 'ctrl' column contains
-    #value of '2' in which case weights remain the same:
-    if (arow['ctrl'] != 2) { 
-      w.m <- w.m + delta * a
+    out <- NULL; xout <- NULL      # Initialize variables
+    w.m <- st$w                    # Initialize weights
+    nw <- length(w.m)              # Calculate number of weights    
+    for(i in 1:nrow(tr)) {         # Run training loop
+        if (arow['ctrl'] == 1) {   # Reset weights?
+            w.m <- st$w
+        }
+        arow <- tr[i, ]            # extract current trial
+        lambda <- arow['t']        # extract teaching signal
+        a <- arow[(st$colskip + 1):(st$colskip + nw)]   # extract inputs
+        suma <- sum(a*w.m)         # Calculate sum of act * weights
+        delta <- st$lr * (lambda - suma)   # Calculte error term
+        if (arow['ctrl'] != 2) {   # Unless weights are frozen...
+            w.m <- w.m + delta * a # ...update weights
+        }
+        out <- rbind(out, suma)    # Record output activation
+        if(xtdo == TRUE) {         # Record weights (if xtdo)
+            xout <- rbind(xout, w.m)
+        }
     }
-
-    #Output for each loop added to output from previous loops:
-    out <- rbind(out, suma)
-    xout <- rbind(xout, w.m)
- }
-
- #Data that model outputs (incl if/else clause for extended output):
-  if(xtdo==TRUE) {
-    ret <- list(out = out, st = w.m, xout = xout)
-  } else {
-    ret <- list(out = out, st = w.m)
-  }
- ret
+    if(xtdo==TRUE) {               # Return appropriate list
+        ret <- list(out = out, st = w.m, xout = xout)
+    } else {
+        ret <- list(out = out, st = w.m)
+    }
+    ret
 }
