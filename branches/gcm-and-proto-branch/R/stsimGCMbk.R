229c145f3b7301563e083c3bdbfb0abcaa2c7a6b
ajwills <ajwills@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-05-09 13:39:09 +0000

    Tidying Rene S's code.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@212 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/gcm-and-proto-branch/R/stsimGCMbk.R
@@ -0,0 +1,122 @@
+## Rene S.
+
+## Main function
+
+stsimGCMbk<-function(st) {
+    ## Add weight for last feature dimension
+    st$weights <- as.numeric(c(st$weights, 1-sum(st$weights)))
+    
+    ## Add choice bias for the last category
+    st$choice_bias <- as.numeric(
+        c(st$choice_bias, 1-sum(st$choice_bias)))
+
+    ## Set up memory strength parameters
+    st$is_memory_pars<-list()
+
+    ## start with all equall memory strengths... 
+    for (cat in 1:st$nCats) {
+        st$is_memory_pars[[cat]] <-
+            c(rep(1, ncol(st$training_items[[cat]])))
+    }
+
+    ## then modify...
+
+    ## equal memory boost for all
+    if (is.atomic(st$mp)) {
+        for (cat in 1:st$nCats) {
+            try(st$is_memory_pars[[cat]][st$memory_items[[cat]]] <-
+                    st$mp, silent = TRUE)
+        }
+    }
+
+    ## item-specific memory boost
+    if (!is.atomic(st$mp)) {
+        for (cat in 1:st$nCats) {
+            try(st$is_memory_pars[[cat]][st$memory_items[[cat]]] <-
+                    st$mp[[cat]], silent = TRUE)
+        }
+    }
+
+    ## Pass to calculation function
+    get_p <- .gcm.predictions(sensitivity = st$sensitivity, 
+                         weights = st$weights, 
+                         choice_bias = st$choice_bias, 
+                         gamma = st$gamma, 
+                         is_memory_pars = st$is_memory_pars, 
+                         r_metric = st$r_metric, 
+                         training_items = st$training_items, 
+                         test_items = st$test_items,
+                         p_shape = st$p,
+                         nCats = st$nCats)
+    ## output <- st
+    ## output$out <- get_p
+    return(get_p)
+}
+
+
+## Predictions Function
+## See Equation 1 in Nosofsky (2011)
+
+## Calls for similarity comparisons between test items and exemplars
+## (gcm.similarity), and then calculates overall summed similarity,
+## returns category choice probabilites.
+
+.gcm.predictions <- function(sensitivity, weights, choice_bias, gamma,
+                            is_memory_pars, r_metric, p_shape,
+                            training_items, test_items, nCats) {
+    ## number of test items
+    nTest <- ncol(test_items)
+    
+    ## matrix for test item predictions (ncol = number of categories,
+    ## nrow = number of test items in order of appearence)
+    predProbs <- matrix(0, ncol = nCats, nrow = nTest)
+
+    for (currentItem in 1:nTest){
+        ## vector holding predicted category response probabilities
+        ## for the current test item
+        summed_similarity <- rep(0, nCats)
+        for (cat in 1:nCats){
+            summed_similarity[cat] <- (
+                sum(
+                    is_memory_pars[[cat]] * (
+                        apply(
+                            training_items[[cat]], 2,
+                            FUN = function(x) {
+                                .gcm.similarity(
+                                    c = sensitivity,
+                                    wt = weights,
+                                    r = r_metric,
+                                    p = p_shape,
+                                    x,
+                                    test_items[,currentItem])
+                            }
+                        )
+                    )
+                )
+            )
+        }
+        ## gamma response determinism
+        summed_similarity <- summed_similarity ^ gamma
+        ## choice biases
+        summed_similarity <- summed_similarity * choice_bias
+        ## summed similarites for the current item and each category
+        predProbs[currentItem,] <- summed_similarity /
+            sum(summed_similarity)
+  }
+  return(predProbs)
+}
+
+## Similarity Calculation
+## See equation 2 and 3 in Nosofsky (2011)
+
+.gcm.similarity <- function(c, wt, r, p, training_item, test_item) {
+    class(training_item) <- "numeric"
+    class(test_item) <- "numeric"
+    ## absolute distance between feature values (Equation 2)
+    tmp<- sqrt((training_item-test_item)^2)
+    ## feature weighted and r-scaled overall distance (Equation 2)    
+    distance<- (sum(wt*tmp^r))^(1/r)
+    ## Sensitivity and p scaled exponential similarity (Equation 3)
+    similarity<-exp(-c*distance^p)
+    return(similarity)
+}
---
+++ branches/gcm-and-proto-branch/unit_tests/test_stsimGCMbk.R
@@ -0,0 +1,114 @@
+## Unit tests for slpGCMbk
+
+## These are for development purposes only and will not appear in the
+## published package.
+
+## Load development code
+source("../R/stsimGCMbk.R")
+
+## Load stored outputs known (believed?) to be correct
+load("test_stsimGCMbk.RData")
+
+## slpGCMbk unit test 1
+
+## Three Categories with 2 Training Items each, and 10 transfer/test
+## items. Each item has three features with four values: memory
+## strength is equal for all exemplars
+
+st<-list(
+    sensitivity = 3,
+    weights = c(.2,.3),
+    choice_bias = c(1/3 , 1/4),
+    gamma = 1,
+    mp = 1,
+    r_metric = 1,
+    p = 1,
+    nCats = 3
+)
+
+## training item definitions 
+st$training_items <- list()
+
+## category 1  -- mid values on average
+st$training_items[[1]] <- matrix(c(1,2,4,2,3,1), ncol=2, nrow=3)
+
+## category 2  -- high values on average
+st$training_items[[2]]<-matrix(c(4,3,4,3,3,2), ncol=2, nrow=3)
+
+## category 3  -- low values on average
+st$training_items[[3]]<-matrix(c(1,1,2,2,1,2), ncol=2, nrow=3) 
+
+## last 4 items (colums) here are 4 new items, those before are
+## old/trained
+st$test_items <- matrix(
+    c(1, 2, 4, 2, 3, 1, 4, 3, 4, 3,
+      3, 2, 1, 1, 2, 2, 1, 2, 1, 1,
+      1, 2, 2, 2, 3, 3, 3, 4, 4, 4),
+    ncol=10, nrow=3) 
+
+## get the resulting predictions for the test items
+
+## columns of the output correspond to category numbers as defined
+## above rows correspond to the column indices of the test_items
+
+t1 <- stsimGCMbk(st)
+if(sum(t1 == ut1) == 30) print("Test 1 passed.")
+
+## Unit test 2
+
+## Same (settings) as above, except: memory strength is 5 times higher
+## for for some exemplars
+
+st$memory_items<-list()
+
+## exemplar 1 in category 1 receives higher memory strength
+st$memory_items[[1]]<-c(1)
+
+## exemplar 2 in category 3 receives higher memory strength
+st$memory_items[[3]]<-c(2)
+
+## for the second category no exemplar receives higher memory
+## strength, and thus can be ommitted
+
+## memory strength is 5 times higher for each of the exemplars defined
+## in $memory_items
+st$mp<-5 
+
+## get predictions
+t2 <- stsimGCMbk(st)
+if(sum(t2 == ut2) == 30) print("Test 2 passed.")
+
+## Unit test 3 
+## Same (settings) as above, except: memory strength is item specific,
+## i.e. memory strength boost is not the same for different exemplars
+st$memory_items<-list()
+
+## exemplars 1 in category 1 receive higher memory strength
+st$memory_items[[1]]<-c(1,2)
+
+## exemplar 1 in category 2 receives higher memory strength
+st$memory_items[[2]]<-c(2)
+
+## no boost in the third category, therefore omitted
+
+st$mp<-list()
+
+## exemplar 1 and 2 in the first category (as defined above) receive a
+## boost of 10 and 5 respectively (this setting should lead to near
+## 100 % probability of putting the first item in the output into
+## category 1, and to a memory-bias for towards putting items into
+## category 1 depending on their similarity)
+st$mp[[1]]<-c(10,5)
+
+
+## exemplar 2 in the second category (as defined above) receives a
+## boost of 3 as $memory_items[[3]] is not defined, mp will be
+## automatically set to 1 there, and can be omitted here
+st$mp[[2]]<-c(3)
+
+
+## get predictions
+t3 <- stsimGCMbk(st)
+if(sum(t3 == ut3) == 30) print("Test 3 passed.")
+
+
