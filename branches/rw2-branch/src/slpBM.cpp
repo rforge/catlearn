ec5a0a439f7234c4d5b19f22c6d4855034451aa4
sspicer <sspicer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-08-29 14:51:26 +0000

    Updates and edits required for Catlearn code freeze.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@275 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/rw2-branch/src/slpBM.cpp
+++ branches/rw2-branch/src/slpBM.cpp
@@ -5,44 +5,48 @@ using namespace Rcpp;
 List slpBM (List st, NumericMatrix tr,
             bool xtdo = false) {
 int i, j, k, nrow = tr.nrow(), ncol = tr.ncol();
-NumericVector w = as<NumericVector>(st["w"]);             // Initial weights of stimuli
+NumericVector w = as<NumericVector>(st["w"]); // Set initial weights.
 int initw = w.size();
 NumericVector wm; wm = (clone(w));
-double lr = as<double>(st["lr"]);                         // The product of learning rate and stimuli salience
-int colskip = as<int>(st["colskip"]);                     // Optional coloumns (e.g block and trial number)
-NumericVector lambda = tr( _, ncol-1);                    // Subset Asymptote of learning
-NumericVector inputs(initw), activ(initw), delta(initw);  // Initialise vectors for stimuli activation, activated stimuli's weights, and change in associative strength respectively
-NumericVector arow, sumET(nrow);                          // Initialise vector for subsetting current trial and for summed error terms
-NumericMatrix xOUT(nrow, initw);                          // Create matrix for extended output
+double lr = as<double>(st["lr"]);       // Learning rate (alpha*beta).
+int colskip = as<int>(st["colskip"]);   // Number of optional columns to skip.
+NumericVector lambda = tr( _, ncol-1);  // Subset asymptote of learning.
+NumericVector inputs(initw), activ(initw), delta(initw); // Initialise vectors for
+                                                         // main loop.
+NumericVector arow, sumET(nrow);        // Initialise vector for subsetting current
+                                        // trial and for summed error terms.
+NumericMatrix xOUT(nrow, initw);        // Create matrix for extended output.
 
 for (i = 0; i < tr.nrow(); ++i) {
-  arow = tr(i, _);                                        // Extract current trial
-  if (i["ctrl"] == 1)                                     // Reset weights at each participant
+  arow = tr(i, _);                      // Extract current trial.
+  if (i["ctrl"] == 1)                   // Reset weights if new participant.
   {
     wm = st["w"];
   }
       for (k = 0; k < initw; ++k) {
-      inputs[k] = arow[colskip+k];                        // Subset stimuli activations from current trial
-      activ[k] = inputs[k] * wm[k];                       // Current stimuli weights according to activations
-      delta[k] = lr * (lambda[i] - activ[k]);             // Calculate the change in associative strength
+      inputs[k] = arow[colskip+k];      // Subset stimuli activations at current trial.
+      activ[k] = inputs[k] * wm[k];     // Generate current stimuli weights.
+      delta[k] = lr * (lambda[i] - activ[k]); // Calc change in associative strength.
       }
-      sumET[i] = sum(activ);                                 // Record output (uses summed error value even though BM uses a seperable error term)
-  if (i["ctrl"] != 2 ) {                                  // Unless weights are frozen
+      sumET[i] = sum(activ);            // Record output (as summed term although BM
+                                        // calculates delta from seperable term).
+  if (i["ctrl"] != 2 ) {                // Unless weights are frozen...
     for (k = 0; k < initw; ++k) {
-    wm[k] += delta[k] * inputs[k];                        // Update weights
+    wm[k] += delta[k] * inputs[k];      // ...update weights.
     }
   }
   if (xtdo) {
-      xOUT(i, _) = wm;                                    // If xtdo true, record updated weights to the row corresponding the current trial
+      xOUT(i, _) = wm;                   // If xtdo = true, record updated weights to
+                                         // relevant row (i.e. trial).
   }
 }
 
 if (xtdo) {
-  return Rcpp::List::create(Rcpp::Named("Summed error terms") = sumET,
-                            Rcpp::Named("Extended Output, where rows represent trials and coloumns stimuli weights") = xOUT,
-                            Rcpp::Named("Last state of the model") = wm);
+  return Rcpp::List::create(Rcpp::Named("suma") = sumET,
+                            Rcpp::Named("xout") = xOUT,
+                            Rcpp::Named("st") = wm);
 } else {
-  return Rcpp::List::create(Rcpp::Named("Summed error terms") = sumET,
-                            Rcpp::Named("The last state of the model") = wm);
+  return Rcpp::List::create(Rcpp::Named("suma") = sumET,
+                            Rcpp::Named("st") = wm);
 }
 }
--- branches/rw2-branch/src/slpRW.cpp
+++ branches/rw2-branch/src/slpRW.cpp
@@ -2,49 +2,52 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-List slpRWs (List st, NumericMatrix tr,
+List slpRW (List st, NumericMatrix tr,
             bool xtdo = false) {
   int i, j, k, nrow = tr.nrow(), ncol = tr.ncol();
-  NumericVector w = as<NumericVector>(st["w"]);             // Initial weights of stimuli
+  NumericVector w = as<NumericVector>(st["w"]); // Set initial weights.
   int initw = w.size();
   NumericVector wm; wm = (clone(w));
-  double lr = as<double>(st["lr"]);                         // The product of learning rate and stimuli salience
-  int colskip = as<int>(st["colskip"]);                     // Optional coloumns (e.g block and trial number)
-  NumericVector lambda = tr( _, ncol-1);                    // Subset Asymptote of learning
-  NumericVector inputs(initw), activ(initw), delta(initw);  // Initialise vectors for stimuli activation, activated stimuli's weights, and change in associative strength respectively
-  NumericVector arow, sumET(nrow);                          // Initialise vector for subsetting current trial and for summed error terms
-  NumericMatrix xOUT(nrow, initw);                          // Create matrix for extended output
+  double lr = as<double>(st["lr"]);       // Learning rate (alpha*beta).
+  int colskip = as<int>(st["colskip"]);   // Number of optional columns to skip.
+  NumericVector lambda = tr( _, ncol-1);  // Subset asymptote of learning.
+  NumericVector inputs(initw), activ(initw), delta(initw);  // Initialise vectors for
+                                                            // main loop.
+  NumericVector arow, sumET(nrow);        // Initialise vector for subsetting current
+                                          // trial and for summed error terms.
+  NumericMatrix xOUT(nrow, initw);        // Create matrix for extended output
   
   for (i = 0; i < tr.nrow(); ++i) {
-    arow = tr(i, _);                                        // Extract current trial
-    if (i["ctrl"] == 1)                                     // Reset weights at each participant
+    arow = tr(i, _);                      // Extract current trial.
+    if (i["ctrl"] == 1)                   // Reset weights if new participant.
     {
       wm = st["w"];
     }
     for (k = 0; k < initw; ++k) {
-      inputs[k] = arow[colskip+k];                        // Subset stimuli activations from current trial
-      activ[k] = inputs[k] * wm[k];                       // Current stimuli weights according to activations
+      inputs[k] = arow[colskip+k];        // Subset stimuli activations at current trial.
+      activ[k] = inputs[k] * wm[k];       // Generate current stimuli weights.
     }
-    sumET[i] = sum(activ);                               // Record output (uses summed error value even though BM uses a seperable error term)
+    sumET[i] = sum(activ);                // Record output (summed error term).
     for (k = 0; k < initw; ++k) {
-      delta[k] = lr * (lambda[i] - sumET[i]);                 // Calculate the change in associative strength
+      delta[k] = lr * (lambda[i] - sumET[i]); // Calc change in associative strength.
     }
-    if (i["ctrl"] != 2 ) {                                  // Unless weights are frozen
+    if (i["ctrl"] != 2 ) {                // Unless weights are frozen...
       for (k = 0; k < initw; ++k) {
-        wm[k] += delta[k] * inputs[k];                        // Update weights
+        wm[k] += delta[k] * inputs[k];    // ...update weights.
       }
     }
     if (xtdo) {
-      xOUT(i, _) = wm;                                    // If xtdo true, record updated weights to the row corresponding the current trial
+      xOUT(i, _) = wm;                    // If xtdo = true, record updated weights to
+                                          // relevant row (i.e. trial).
     }
   }
   
   if (xtdo) {
-    return Rcpp::List::create(Rcpp::Named("Summed error terms") = sumET,
-                              Rcpp::Named("Extended Output, where rows represent trials and coloumns stimuli weights") = xOUT,
-                              Rcpp::Named("Last state of the model") = wm);
+    return Rcpp::List::create(Rcpp::Named("suma") = sumET,
+                              Rcpp::Named("xout") = xOUT,
+                              Rcpp::Named("st") = wm);
   } else {
-    return Rcpp::List::create(Rcpp::Named("Summed error terms") = sumET,
-                              Rcpp::Named("The last state of the model") = wm);
+    return Rcpp::List::create(Rcpp::Named("suma") = sumET,
+                              Rcpp::Named("st") = wm);
   }
 }
