0b65d3f68dac8195b0eacc2e0295824329597311
ajwills <ajwills@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-11-28 14:27:50 +0000

    Updated set of unit tests, ready for version 0.5 release.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@306 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- unit_tests/test_slpdiva.R
+++ unit_tests/test_slpdiva.R
@@ -1,8 +1,8 @@
 test.slpDIVA <- function() {
-    # Test based on a Type IV Shepard et al.(1961) problem.
+    ## Test based on a Type IV Shepard et al.(1961) problem.
     load("data/test_slpdiva.RData")
     tout <- slpDIVA(st,tr)
-    sum((out$out - tout$out)^2) == 0
+    !sum((tout$out[,1] - cor.out)^2)
 }
 
     
