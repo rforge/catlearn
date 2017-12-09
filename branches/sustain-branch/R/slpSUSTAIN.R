e84bda9976249b43e323a38a729ae9acade35941
lenarddome <lenarddome@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-12-09 13:43:36 +0000

    updated todo simplified code a little (recalculating cluster output after adding new cluster)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@309 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/sustain-branch/R/slpSUSTAIN.R
+++ branches/sustain-branch/R/slpSUSTAIN.R
@@ -8,7 +8,7 @@
 }
 
 .calc.distances <- function(input, cluster, fac.dims, fac.na) {
-  mu <- matrix(0, nrow = nrow(cluster), 
+  mu <- matrix(0, nrow = nrow(cluster),
                ncol = length(unique(fac.dims)))
   for (k in 1:nrow(cluster)) {
     mu[k, ] <- as.vector(tapply(abs(input - cluster[k, fac.na]),
@@ -17,13 +17,8 @@
   return(mu)
 }
 
-.output.store <- function(prob.o, que.pattern) {
-  inp <- list(que.pattern, prob.o)
-  ord <- sort(rep(seq(ncol(prob.o)), length(inp)))
-  n <- max(unlist(lapply(inp, length)))
-  prob.xout <- sapply(inp, `[`, 1:n)
-  browser()  
-  return(prob.xout)
+cluster.activation <- function(lambda, r, beta, mu.neg, mu.pos){
+
 }
 
 slpSUSTAIN <- function(st, tr, xtdo = FALSE) {
@@ -46,7 +41,7 @@ slpSUSTAIN <- function(st, tr, xtdo = FALSE) {
   if(length(fac.queried) == 0){
     fac.queried <- fac.na
   }
-  
+
   # Setting up environment
   # Arrays for xout, and to store Euler's number (for readability)
   e <- exp(1)
@@ -59,9 +54,9 @@ slpSUSTAIN <- function(st, tr, xtdo = FALSE) {
  #' trial Import current trial
  #' if Update parameters in case of a new participant or any other reasons
  #' input Set up stimulus representation
- #browser()
+ # browser()
     trial <- tr[i, ]
-    if (trial['ctrl'] != 0) {
+    if (trial['ctrl'] == 1) {
     cluster <- matrix(as.vector(trial[st$colskip:(length(trial)-1)]),
                       nrow = 1)
     w <- st$w
@@ -72,17 +67,18 @@ slpSUSTAIN <- function(st, tr, xtdo = FALSE) {
     input <- as.vector(trial[st$colskip:(st$colskip + sum(st$dims) - 1)])
  # Calculate distances of stimulus from each cluster's position
     mu <- .calc.distances(input, cluster, fac.dims, fac.na)
-   
+
  # The activations of clusters
  #' H.nom Set up the nominator without the sums
  #' H.act Calculate the activation for each cluster
  #' H.out Output activations after cluster competition
- #' C.out Activations of output units of the quired dimension'
+ #' C.out Activations of output units of the quired dimension
     mu.product.neg <- sweep(mu, MARGIN = 2, -lambda, `*`)
     mu.product.pos <- sweep(mu, MARGIN = 2, lambda, `*`)
     H.nom <- sweep(e ^ (mu.product.neg), MARGIN = 2, lambda ^ st$r, `*`)
     H.act <- apply(H.nom, MARGIN = 1, sum) / sum(lambda ^ st$r)  # Equation 5
     H.out <- (H.act ^ st$beta / sum(H.act^st$beta)) * H.act  # Equation 6
+
     H.out[which(H.act < max(H.act))] <- 0
     C.out <- w[which.max(H.act), ] * H.out[which.max(H.act)]  # Equation 7
     prob.r <-  .prob.response(C.out[fac.queried], st$d)  ## Equation 8
@@ -103,46 +99,51 @@ slpSUSTAIN <- function(st, tr, xtdo = FALSE) {
  #' Add new cluster's weights and set them to zero
  #' Add new stim's distance (which is zero on all dimensions)
  #' Recompute activations of the clusters (including the recruited cluster)
- #' with clusters' competition
+ #' with clusters competition
     if (trial["t"] == 1) {
        ifelse(length(unique(C.out[fac.queried])) == 1,
               t.queried <- which(target[fac.queried] == 1),
               t.queried <- which.max(C.out[fac.queried]))
       #browser()
- # Equation 10
-      if (target[fac.queried][t.queried] < 1) {
-        cluster <- rbind(cluster,
-                         as.vector(trial[st$colskip:(length(trial)-1)]))
-        w <-  rbind(w, rep(0, length(st$w)))
-        mu <- rbind(mu, vector(mode = "numeric",
-                               length = length(st$dims)))
-        mu.product.neg <- sweep(mu, MARGIN = 2, -lambda, `*`)
-        mu.product.pos <- sweep(mu, MARGIN = 2, lambda, `*`)
-        H.nom <- sweep(e ^ (mu.product.neg), MARGIN = 2,
-                       lambda ^ st$r, `*`)
-        H.act <- apply(H.nom, MARGIN = 1, sum) / sum(lambda ^ st$r)
-        H.out <- (H.act ^ st$beta / sum(H.act^st$beta)) * H.act
-        H.out[which(H.act < max(H.act))] <- 0
-        }
-      } else {
- # Equation 11
-      if (H.act[which.max(H.act)] < st$tau) {
-        cluster <- rbind(cluster,
-                         as.vector(trial[st$colskip:(length(trial)-1)]))
-        w <-  rbind(w, rep(0, length(st$w)))
-        mu <- rbind(mu, vector(mode = "numeric", length = length(st$dims)))
-        H.nom <- sweep(e ^ (-lambda * mu), MARGIN = 2, lambda ^ st$r, `*`)
-        H.act <- apply(H.nom, MARGIN = 1, sum) / sum(lambda ^ st$r)
-        H.out <- (H.act ^ st$beta / sum(H.act^st$beta)) * H.act
-        H.out[which(H.act < max(H.act))] <- 0
-      }
     }
+ # Equation 10
+    if (target[fac.queried][t.queried] < 1 || H.act[which.max(H.act)] < st$tau) {
+      cluster <- rbind(cluster,
+                       as.vector(trial[st$colskip:(length(trial)-1)]))
+      w <-  rbind(w, rep(0, length(st$w)))
+      mu <- rbind(mu, vector(mode = "numeric",
+                             length = length(st$dims)))
+      mu.product.neg <- sweep(mu, MARGIN = 2, -lambda, `*`)
+      mu.product.pos <- sweep(mu, MARGIN = 2, lambda, `*`)
+      H.nom <- sweep(e ^ (mu.product.neg), MARGIN = 2,
+                     lambda ^ st$r, `*`)
+      H.act <- apply(H.nom, MARGIN = 1, sum) / sum(lambda ^ st$r)
+      H.out <- (H.act ^ st$beta / sum(H.act^st$beta)) * H.act
+      H.out[which(H.act < max(H.act))] <- 0
+      }
+#      }
+#       else {
+ # Equation 11
+#      if (H.act[which.max(H.act)] < st$tau) {
+#        cluster <- rbind(cluster,
+#                         as.vector(trial[st$colskip:(length(trial)-1)]))
+#        w <-  rbind(w, rep(0, length(st$w)))
+#        mu <- rbind(mu, vector(mode = "numeric",
+#                               length = length(st$dims)))
+#        mu.product.neg <- sweep(mu, MARGIN = 2, -lambda, `*`)
+#        mu.product.pos <- sweep(mu, MARGIN = 2, lambda, `*`)
+#        H.nom <- sweep(e ^ (mu.product.neg), MARGIN = 2,
+#                       lambda ^ st$r, `*`)
+#        H.act <- apply(H.nom, MARGIN = 1, sum) / sum(lambda ^ st$r)
+#        H.out <- (H.act ^ st$beta / sum(H.act^st$beta)) * H.act
+#        H.out[which(H.act < max(H.act))] <- 0
+#      }
+#    }
 
  # Store Output Information
    win <- which.max(H.act)
-   
-   prob.o <- rbind(prob.o, prob.r)
    cout.o <- rbind(cout.o, C.out[fac.queried])
+
  # Update
  #' Only update the winning cluster
  #' Lambdas are updated by the winning cluster
@@ -154,15 +155,17 @@ slpSUSTAIN <- function(st, tr, xtdo = FALSE) {
    cluster[win, ][fac.na] <-
      cluster[win, ][fac.na] + (st$eta * (input - cluster[win, ][fac.na]))
  # Equation 13
-   lambda <- lambda + (st$eta * e ^ (mu.product.neg[win, ]) * 
-                         (1 - mu.product.pos[win, ]))
+   lambda <-
+     lambda + (st$eta * e ^ (mu.product.neg[win, ]) *
+     (1 - mu.product.pos[win, ]))
  # Equation 14 - one-layer delta learning rule (Widrow & Hoff, 1960)
     w[win, ] <- w[win, ] + (st$eta * (target - C.out) * H.out[win])
     xout[i] <- win
+    que[i] <- which(target[fac.queried]==1)
     }
   }
   # browser()
-  prob.xout <- .output.store(prob.o, tr[, st$colskip - 1 + fac.queried])
+  prob.xout <- cbind(prob.o, que, xout)
   mode <- rbind(c(1:nrow(cluster)),
                 matrix(table(xout), nrow = 1))
   mean <- mean(mode[1, ])
