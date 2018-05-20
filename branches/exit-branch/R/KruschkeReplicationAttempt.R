a304a69e88f1fee13234a6dd34d63719981def0d
rene <rene@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-05-20 18:21:33 +0000

    slpEXIT 20-5-2018
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@341 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/exit-branch/R/KruschkeReplicationAttempt.R
@@ -0,0 +1,285 @@
+#rm(list=ls())
+#library(catlearn)
+## Kruschke & Blair 2000 ## Experiment 1
+## create sequence of trials tr as reported
+## note x10 is the bias cue 
+if (T) {
+ncolumns<-16
+train1<-as.data.frame(matrix(0,ncol=ncolumns,nrow=2))
+colnames(train1)<-c("x1","x2","x3","x4","x5","x6","x7","x8","x9", "t1","t2","t3","t4","t5","t6","stim")
+train1[,c("t1","t2","t3","t4","t5","t6")]<--1
+train1[1,c("x1","t1")]<-1;      train1[1,c("stim")]<-"A"
+train1[2,c("x4","t3")]<-1;      train1[2,c("stim")]<-"D"
+
+train2<-as.data.frame(matrix(0,ncol=ncolumns,nrow=3))
+colnames(train2)<-c("x1","x2","x3","x4","x5","x6","x7","x8","x9", "t1","t2","t3","t4","t5","t6","stim")
+train2[,c("t1","t2","t3","t4","t5","t6")]<--1
+train2[1,c("x1","x2","t1")]<-1; train2[1,c("stim")]<-"AB"
+train2[2,c("x4","t3")]<-1;      train2[2,c("stim")]<-"D"
+train2[3,c("x8","x9","t6")]<-1; train2[3,c("stim")]<-"HI"
+
+block1<-as.data.frame(matrix(0,ncol=ncolumns,nrow=11))
+colnames(block1)<-c("x1","x2","x3","x4","x5","x6","x7","x8","x9", "t1","t2","t3","t4","t5","t6","stim")
+block1[,c("t1","t2","t3","t4","t5","t6")]<--1
+block1[1,c("x1","x2")]<-1; block1[1,c("stim")]<-"AB"
+block1[2,c("x4")]<-1;      block1[2,c("stim")]<-"D"
+block1[3,c("x8","x9")]<-1; block1[3,c("stim")]<-"HI"
+block1[4,c("x2","x8")]<-1; block1[4,c("stim")]<-"BH"
+block1[5,c("x2","x9")]<-1; block1[5,c("stim")]<-"BI"
+block1[6,c("x2","x4")]<-1; block1[6,c("stim")]<-"BD"
+block1[7,c("x1","x4")]<-1; block1[7,c("stim")]<-"AD"
+block1[8,c("x1","x8")]<-1; block1[8,c("stim")]<-"AH"
+block1[9,c("x1","x9")]<-1; block1[9,c("stim")]<-"AI"
+block1[10,c("x4","x8")]<-1; block1[10,c("stim")]<-"DH"
+block1[11,c("x4","x9")]<-1; block1[11,c("stim")]<-"DI"
+
+train3<-as.data.frame(matrix(0,ncol=ncolumns,nrow=6))
+colnames(train3)<-c("x1","x2","x3","x4","x5","x6","x7","x8","x9", "t1","t2","t3","t4","t5","t6","stim")
+train3[,c("t1","t2","t3","t4","t5","t6")]<--1
+train3[1,c("x1","t1")]<-1;      train3[1,c("stim")]<-"A"
+train3[2,c("x4","t3")]<-1;      train3[2,c("stim")]<-"D"
+train3[3,c("x7","t5")]<-1;      train3[3,c("stim")]<-"G"
+train3[4,c("x1","x2","x3","t2")]<-1;      train3[4,c("stim")]<-"ABC"
+train3[5,c("x4","x5","x6","t4")]<-1;      train3[5,c("stim")]<-"DEF"
+train3[6,c("x7","x8","x9","t6")]<-1;      train3[6,c("stim")]<-"GHI"
+
+block2<-as.data.frame(matrix(0,ncol=ncolumns,nrow=20))
+colnames(block2)<-c("x1","x2","x3","x4","x5","x6","x7","x8","x9", "t1","t2","t3","t4","t5","t6","stim")
+block2[,c("t1","t2","t3","t4","t5","t6")]<--1
+block2[1,c("x1")]<-1;               block2[1,c("stim")]<-"A"
+block2[2,c("x1","x2","x3")]<-1;     block2[2,c("stim")]<-"ABC"
+block2[3,c("x4")]<-1;               block2[3,c("stim")]<-"D"
+block2[4,c("x4","x5","x6")]<-1;     block2[4,c("stim")]<-"DEF"
+block2[5,c("x7")]<-1;               block2[5,c("stim")]<-"G"
+block2[6,c("x7","x8","x9")]<-1;     block2[6,c("stim")]<-"GHI"
+block2[7,c("x2","x5")]<-1;          block2[7,c("stim")]<-"BE"
+block2[8,c("x2","x6")]<-1;          block2[8,c("stim")]<-"BF"
+block2[9,c("x3","x5")]<-1;          block2[9,c("stim")]<-"CE"
+block2[10,c("x3","x6")]<-1;         block2[10,c("stim")]<-"CF"
+block2[11,c("x2","x8")]<-1;         block2[11,c("stim")]<-"BH"
+block2[12,c("x2","x9")]<-1;         block2[12,c("stim")]<-"BI"
+block2[13,c("x3","x8")]<-1;         block2[13,c("stim")]<-"CH"
+block2[14,c("x3","x9")]<-1;         block2[14,c("stim")]<-"CI"
+block2[15,c("x1","x2")]<-1;         block2[15,c("stim")]<-"AB"
+block2[16,c("x1","x3")]<-1;         block2[16,c("stim")]<-"AC"
+block2[17,c("x4","x5")]<-1;         block2[17,c("stim")]<-"DE"
+block2[18,c("x4","x6")]<-1;         block2[18,c("stim")]<-"DF"
+block2[19,c("x7","x8")]<-1;         block2[19,c("stim")]<-"GH"
+block2[20,c("x7","x9")]<-1;         block2[20,c("stim")]<-"GI"
+
+## number of trials overall
+ntrials<-20*2+20*3+1*11+15*6+1*20
+
+block_index<-c(rep("training1",20*2),rep("training2",20*3),rep("blocking",1*11),rep("training3",15*6),rep("attenuation",1*20))
+## set up all training trials
+tr<-as.data.frame(matrix(0,ncol=ncolumns+2,nrow=ntrials))
+colnames(tr)<-c("ctrl", "x1","x2","x3","x4","x5","x6","x7","x8","x9", "t1","t2","t3","t4","t5","t6","stim", "block")
+tr$block<-block_index
+##train1
+tmp<-as.data.frame(matrix(0,ncol=ncolumns,nrow=20*2))
+for (i in 1:20)  tmp[(1:2)+(i-1)*2,]<-train1[sample(1:2,2),]
+tr[tr$block=="training1",2:(ncolumns+1)]<-tmp
+##train2
+tmp<-as.data.frame(matrix(0,ncol=ncolumns,nrow=20*3))
+for (i in 1:20)  tmp[(1:3)+(i-1)*3,]<-train2[sample(1:3,3),]
+tr[tr$block=="training2",2:(ncolumns+1)]<-tmp
+##test1 (blocking)
+tmp<-as.data.frame(matrix(0,ncol=ncolumns,nrow=1*11))
+for (i in 1:1)  tmp[(1:11)+(i-1)*11,]<-block1[sample(1:11,11),]
+tr[tr$block=="blocking",2:(ncolumns+1)]<-tmp
+##train3 
+tmp<-as.data.frame(matrix(0,ncol=ncolumns,nrow=15*6))
+for (i in 1:15)  tmp[(1:6)+(i-1)*6,]<-train3[sample(1:6,6),]
+tr[tr$block=="training3",2:(ncolumns+1)]<-tmp
+##test2 
+tmp<-as.data.frame(matrix(0,ncol=ncolumns,nrow=1*20))
+for (i in 1:1)  tmp[(1:20)+(i-1)*20,]<-block2[sample(1:20,20),]
+tr[tr$block=="attenuation",2:(ncolumns+1)]<-tmp
+
+## set ctrl values
+tr$ctrl<-0
+tr$ctrl[1]<-1
+tr$ctrl[tr$block=="blocking"]<-2
+tr$ctrl[tr$block=="attenuation"]<-2
+}
+tr
+## best fit values for EXIT with attention shifting 
+## as reported in Kruschke, 2001 
+st<-list(nFeat=9, 
+         nCat=6, 
+         phi=4.43, 
+         c=.348, 
+         P=1.07,
+         l_gain=1.27, 
+         l_weight=.316,
+         l_ex=.0121, 
+         beta=.001,
+         iterations=10,
+         beta=1,
+         eta=1)
+
+st$exemplars<-
+    rbind(train1[train1[,"stim"]=="A",1:st$nFeat],
+          train1[train1[,"stim"]=="D",1:st$nFeat],
+          train3[train3[,"stim"]=="G",1:st$nFeat],
+          train2[train2[,"stim"]=="AB",1:st$nFeat],
+          train2[train2[,"stim"]=="HI",1:st$nFeat],
+          train3[train3[,"stim"]=="ABC",1:st$nFeat],
+          train3[train3[,"stim"]=="DEF",1:st$nFeat],
+          train3[train3[,"stim"]=="GHI",1:st$nFeat])
+rownames(st$exemplars)<-c("A","D","G","AB","HI","ABC","DEF","GHI")
+
+st$w_exemplars<-st$exemplars
+st$w_exemplars[]<-0
+st$w_in_out<-matrix(0,st$nCat,st$nFeat)
+
+
+predictions<-as.data.frame(matrix(NA,ncol=8,nrow=ntrials))
+colnames(predictions)<-c("block","stim","t1","t2","t3","t4","t5","t6")
+predictions$stim<-tr$stim;predictions$block<-tr$block
+predictions[,c("t1","t2","t3","t4","t5","t6")]<-
+    round(slp_EXITrs(st,tr, xtdo=F)$response_probabilities,3)
+
+## The Kruschke Table (Kruschke & Blair,2000)
+## Human Data
+# Symp	1	    2	    3   	4   	5   	6
+# BH/BI	15	    6.3	    3.8	    6.3	    10	    58.8
+# AB	81.3	3.8	    3.8	    3.8	    5	    2.5
+# D 	2.5	    0	    96.3	1.3	    0   	0
+# HI	1.3	    0	    0	    1.3	    2.5	    95
+# BD	15	    3.8	    66.3	6.3	    2.5	    6.3
+# AD	42.5	13.8	30	    2.5	    11.3	0
+# AH/AI	65	    1.3	    1.3	    5	    3.8	    23.8
+# DH/DI	2.5	    6.3	    43.8	5	    10	    32.5
+
+### Corresponding Model Predictions
+# BH/BI	6.60	6.50	6.50	6.50	6.50	67.40
+# AB	80.30	3.90	3.90	3.90	3.90	3.90
+# D	    1.10	1.10	94.40	1.10	1.10	1.10
+# HI	1.20	1.20	1.20	1.20	1.20	93.80
+# BD	5.80	5.70	71.20	5.70	5.70	5.70
+# AH/AI	54.10	3.90	3.90	3.90	3.90	30.20
+# AD	52.00	3.70	33.10	3.70	3.70	3.70
+# DH/DI	4.40	4.40	44.40	4.40	4.40	38.10
+
+## And the current slpEXIT predictions
+## blocking overall
+## please note the order of symptoms differs % percent is in decimals
+predictions[predictions$block=="blocking",][1:11,2:8]
+
+
+
+##Attenuation
+## Human Data
+# BE/BF	2.50	22.50	2.50	58.10	3.10	11.30
+# A	    94.40	1.30	1.30	1.30	0.60	1.30
+# ABC	13.80	72.50	0.60	4.70	2.50	5.00
+# D	    3.10	0.60	93.80	1.30	0.00	1.30
+# DEF	1.30	5.00	12.50	72.50	2.50	6.30
+# G	    0.60	0.00	1.90	0.60	95.60	1.30
+# GHI	1.30	6.30	2.50	5.60	6.90	77.50
+# CE/CF	1.30	39.40	5.00	42.50	3.10	8.80
+# BH/BI	1.30	21.90	0.60	6.30	3.80	66.30
+# CH/CI	1.90	48.80	1.90	3.10	5.00	39.40
+# AB	56.90	27.50	3.10	4.40	0.60	7.50
+# AC	34.40	56.30	1.30	1.30	3.10	3.80
+# DE/DF	1.30	6.30	31.30	51.90	3.10	6.30
+# GH/GI	0.60	4.40	3.80	3.80	34.40	53.10
+
+## Reported Model Predictions
+# BE/BF	5.40	26.80	5.40	51.50	5.40	5.40
+# A	    94.40	1.10	1.10	1.10	1.10	1.10
+# ABC	13.70	72.40	3.50	3.50	3.50	3.50
+# D	    1.10	1.10	94.40	1.10	1.10	1.10
+# DEF	3.30	3.30	12.40	74.20	3.30	3.30
+# G	    1.10	1.10	1.10	1.10	94.30	1.10
+# GHI	2.60	2.60	2.60	2.60	8.10	81.40
+# CE/CF	4.40	47.80	4.40	34.50	4.40	4.40
+# BH/BI	4.80	23.40	4.80	4.80	4.30	57.90
+# CH/CI	4.10	44.30	4.10	4.10	3.80	39.60
+# AB	53.30	26.90	4.90	4.90	4.90	4.90
+# AC	28.60	55.80	3.90	3.90	3.90	3.90
+# DE/DF	4.50	4.50	34.40	47.40	4.50	4.50
+# GH/GI	4.20	4.20	4.20	4.20	27.80	55.50
+
+
+## slpExit predictions
+## please note order of symptoms differs; and % in decimals
+predictions[predictions$block=="attenuation",2:8]
+
+
+
+
+
+## best fit values for EXIT without attention shifting 
+## as reported in Kruschke, 2001 
+st<-list(nFeat=9, 
+         nCat=6, 
+         phi=4.43, 
+         c=.348, 
+         P=1.07,
+         l_gain=0, 
+         l_weight=.316,
+         l_ex=.0121, 
+         beta=.001,
+         iterations=10,
+         eta=1)
+
+st$exemplars<-
+    rbind(train1[train1[,"stim"]=="A",1:st$nFeat],
+          train1[train1[,"stim"]=="D",1:st$nFeat],
+          train3[train3[,"stim"]=="G",1:st$nFeat],
+          train2[train2[,"stim"]=="AB",1:st$nFeat],
+          train2[train2[,"stim"]=="HI",1:st$nFeat],
+          train3[train3[,"stim"]=="ABC",1:st$nFeat],
+          train3[train3[,"stim"]=="DEF",1:st$nFeat],
+          train3[train3[,"stim"]=="GHI",1:st$nFeat])
+rownames(st$exemplars)<-c("A","D","G","AB","HI","ABC","DEF","GHI")
+
+st$w_exemplars<-st$exemplars
+st$w_exemplars[]<-0
+st$w_in_out<-matrix(0,st$nCat,st$nFeat)
+
+
+predictions<-as.data.frame(matrix(NA,ncol=8,nrow=ntrials))
+colnames(predictions)<-
+    c("block","stim","t1","t2","t3","t4","t5","t6")
+predictions$stim<-tr$stim; predictions$block<-tr$block
+predictions[,c("t1","t2","t3","t4","t5","t6")]<-
+    round(slp_EXITrs(st,tr,xtdo=F)$response_probabilities,2)
+
+## blocking (see figure 2 in Kruschke, 2001,
+##Model predictions
+# BH/BI	18.10	7.30	7.30	7.30	7.30	52.70	52.7
+# AB	92.80	1.40	1.40	1.40	1.40	1.40	1.4
+# D	    1.20	1.20	93.90	1.20	1.20	1.20	1.2
+# HI	1.80	1.80	1.80	1.80	1.80	91.20	91.2
+# BD	14.20	5.70	62.80	5.70	5.70	5.70	5.7
+# AH/AI	69.90	2.70	2.70	2.70	2.70	19.30	19.3
+# AD	63.50	2.40	26.70	2.40	2.40	2.40	2.4
+# DH/DI	4.50	4.50	49.40	4.50	4.50	32.50	32.5
+
+## slpExit here
+predictions[predictions$block=="blocking",2:8]
+
+##(non-existent) attenuation
+## Model predictions
+# BE/BF	6.70	37.70	3.70	37.70	7.10	7.10
+# A	    90.00	3.50	1.60	1.60	1.60	1.60
+# ABC	11.30	69.30	4.80	4.80	4.80	4.80
+# D	    1.60	1.60	90.30	3.40	1.60	1.60
+# DEF	4.90	4.90	9.50	70.70	4.90	4.90
+# G	    1.90	1.90	1.90	1.90	89.80	2.50
+# GHI	2.10	2.10	2.10	2.10	4.60	87.10
+# CE/CF	2.80	39.30	3.80	39.30	7.40	7.40
+# BH/BI	4.30	24.20	4.50	4.50	2.80	59.60
+# CH/CI	1.80	24.90	4.70	4.70	2.90	61.20
+# AB	41.70	39.10	4.80	4.80	4.80	4.80
+# AC	22.40	52.00	6.40	6.40	6.40	6.40
+# DE/DF	5.90	5.90	28.50	47.90	5.90	5.90
+# GH/GI	4.10	4.10	4.10	4.10	20.90	62.70
+
+## slpExit here
+predictions[predictions$block=="attenuation",2:8]
+
+
--- branches/exit-branch/R/slpEXITrs.R
+++ branches/exit-branch/R/slpEXITrs.R
@@ -1,181 +1,220 @@
 ## Author: René Schlegelmilch
     
-slp_EXIT<-function(st, tr) {
+slp_EXITrs<-function(st, tr,xtdo=FALSE) {
     
-    out<-.subfunction(st,tr)
+    out<-.subfunction(st,tr, extendx=xtdo)
     return(out)
    
 }
 
-.subfunction<- function(st, tr){
+.subfunction<- function(st, tr, extendx){
     
     ## Preparation of processing vars
 
-    ## column indices of the features
-    colFeat1<-grep("x1", colnames(tr))
+    ## first column indices of the features
+    colFeat1<-grep("x1$", colnames(tr))
 
-    ## and the teacher values
-    colt1<-grep("t1", colnames(tr))
+    ## and first column of the teacher values
+    colt1<-grep("t1$", colnames(tr))
     
-    ## set up response probability matrix
+    ## set up ouptut matrices
+    ## response probability matrix will be the default output
     probs_out<-matrix(0, ncol=st$nCat, nrow=nrow(tr))
+    
+    ## extended output
+    if (extendx==T){
+        E4_exemplar_weights<-list()
+        weights_in_out<-list()
+        gains<-matrix(0,ncol=st$nFeat,nrow=nrow(tr))
+        attention<-matrix(0,ncol=st$nFeat,nrow=nrow(tr))
+        bias_weight<-matrix(0,ncol=st$nCat ,nrow=nrow(tr))
+    }
+    
+    
+    ## Initialization of bias to category nodes
+    w_bias<-rep(0,st$nCat)
+    
     
     ## go through all the trials and apply model
     for(j in 1:nrow(tr)){
         
         ## first define indicator variables for correct cats
-        ## (in case {tr} does not provide this info, e.g. in 
-        ## test trials without feedback)
+        ## cCat: index of present cat; eCat: absent cats
         cCat<-eCat<-c()
         
-        ## cCat: index(es) of correct cats; eCat: incorrect cats
         ## ("try", in case there are no cats e.g. during test trials)
         try(cCat<-which(tr[j,colt1:(colt1+st$nCat-1)] %in% 1),
             silent==TRUE)
         try(eCat<-which(tr[j,colt1:(colt1+st$nCat-1)] %in% -1),
             silent==TRUE)
         
-        ## teachers signals:
+        ## teaching signals:
         ## The EXIT paper simply states:
         ## t=0 if outcome is absent and t=1 if outcome present.
         teacher<- matrix(0, ncol=st$nCat)
         teacher[cCat]<- 1
         teacher[eCat]<- 0
-        ## humble teacher option?
-        # teacher[cCat]<- max(1,out_act[cCat])
-        # teacher[eCat]<- min(0,out_act[eCat])
-        
         
         ## Extract input activations for current trial 
         ## (i.e. which feature is present?)
-        a_in<-matrix(tr[j,colFeat1:(colFeat1+st$nFeat-1)], 
-                           ncol=st$nFeat)
-        
-        ## number of currently presented features
-        nPresFeat<-sum(a_in)
+        a_in<-as.numeric(tr[j,colFeat1:(colFeat1+st$nFeat-1)])
         
         ## calculate exemplar activation a_ex(x)
         ## with minkowski metric Equation (3)
-        ## for every exemplar x in memory
-        ## note: for the first trials it is assumed,
-        ## that the exemplars are in memory
         a_ex<-exp(-st$c*rowSums(
             abs(t(
                 t(st$exemplars)-
                     as.numeric(a_in)))
         ))
         
-        ## calculate current activation of gain nodes g
-        ## Equation (4) 
-        g<-a_in*exp(colSums(st$w_exemplars*a_ex))
         
-        ## is this a complete reset trial? (ctrl==1)
+        ## is this a reset trial? (ctrl==1)
         ## reset exemplar weight and gain nodes
         if (tr[j,"ctrl"]==1){
         w_exemplars<-st$exemplars
         w_exemplars[]<-0
-        g<-a_in*exp(colSums(w_exemplars*a_ex))
         w_in_out<-matrix(0,st$nCat,st$nFeat)
+        ## just for better understanding and debugging here
+        ## rownames(w_in_out)<-c("t1","t2","t3","t4","t5","t6")
         }
         
         ## user defined initialization?
         if (tr[j,"ctrl"]==3){
-            w_exemplars<-st$exemplars
-            g<-a_in*exp(colSums(w_exemplars*a_ex))
-            w_in_out<-st$w_in_out
+            w_exemplars<-st$ex_weights_init
+            w_in_out<-st$w_io_init
         }
+        
+        ## calculate current activation of gain nodes g
+        ## Equation (4) 
+        g<-a_in*exp(colSums(w_exemplars*a_ex))
         ## negative gains are set to 0
         g[g<0]<-0
         
-        
-        ## calculate attention strengths alpha_i
-        ## Equation (5) in Kruschke 2001
+        ## calculate current attention strengths alpha_i
+        ## Equation (5) 
+        ## Note: used for updates in Equations 9 and 10
         alpha_i<-g/((sum(g^st$P))^(1/st$P))
         
         ## calculate category activation
-        ## Equation (1) 
-        out_act<-(a_in*alpha_i)%*%t(w_in_out)
-        
-        ## and category probability stored
-        ## ( before learning re-iteration)
+        ## Equation (1) (or 40 with bias)
+        ## Note: used for updates in Equations 9 and 10
+        out_act<-(a_in*alpha_i)%*%t(w_in_out)+w_bias
+       
+        ## and category probability
+        ## Equation (2) (or 39 with bias)
         probs<-exp(out_act*st$phi)/sum(exp(out_act*st$phi))
         
-        ## is this a frozen learning trial?
+        ## is this a frozen learning trial? (ctrl==2)
         if( tr[j,"ctrl"]!=2) {
         
-        ## Gradient of error for attention shift
+        
+        ## Attention Shifting
         ## Equation (8)
         ## iterates multiple times by an arbitrary number...
-        ## (could also be only one equation, but for readability)
+        g_inits<-g
         for (k in 1:st$iterations) {
         
-            if (k>1) {
             ## calculate attention strengths alpha_i
-            ## Equation (5) in Kruschke 2001
-            alpha_i<-g/((sum(g^st$P))^(1/st$P))
+            ## Note: calculated in each iteration according to Kruschke
+            ## Equation (5) 
+            alpha_i2<-g/((sum(g^st$P))^(1/st$P))
             
             ## calculate category activation
+            ## Note: calculated in each iteration according to Kruschke
             ## Equation (1) 
-            out_act<-(a_in*alpha_i)%*%t(w_in_out)
-
-            }
+            out_act2<-(a_in*alpha_i2)%*%t(w_in_out)
 
             ### Update exemplar gains, attention weights 
             ### and exemplar node weights:::::::::::::::
-            
-            ## store gain init_values for later adjustment in
-            ##  Equation (10)
-            gain_inits<-g
-            
+
             ## first term teacher values
-            E1<-(teacher-out_act)
+            E1<-(teacher-out_act2)
             
-            ## second term (w*a_in - a_out*alpha)
-            E2<-t(t(w_in_out)*as.numeric(a_in))-
-                t(out_act)%*%as.numeric(alpha_i^(st$P-1))
+            ## second term (wI*a_inI - alpha_I*a_out)
+            E2<-t(as.numeric(a_in[a_in==1])*t(w_in_out[,a_in==1])-
+                as.numeric(alpha_i2[a_in==1]^(st$P-1))%*%(out_act2))
+            
+            ## Terms E1 and E2 are calculated for each current input I
+            ## and then summed accross category nodes
+            Ex<-E1%*%(E2)
             
             ## divided by 3rd term
             E3<-sum(g^st$P)^(1/st$P)
             
             ## Full equation then:
-            gain_delta<-st$l_gain*(as.numeric(E1)%*%(E2/E3))
+            gain_delta<-st$l_gain*(Ex/E3)
             
-            ## adjust g
-            g<-g+gain_delta
+            ## adjust g for current Input I
+            g[a_in==1]<-g[a_in==1]+gain_delta
             g[g<0]<-0
+            ## in the next iteration g is used for re-calculating
+            ## alpha i and output activation
         }
         
+        ## commiting the changed variables to the 
+        ## next steps increases learning speed
+        ## Without this step, predicted blocking probs are "lower"
+        ## Whithout this step, attenuation is hardly predicted
+        ## try it yourself
+        out_act<-out_act2
+        alpha_i<-alpha_i2
+        
+        ## Learning of Associations
         ## Gradient of error for weight change
+        ## calculated for K (... I assume this means the present category)
+        ## and I (present Input)
         ## Equation (9)
+        weight_delta<-st$l_weight*(teacher[cCat]-out_act[cCat])*
+                        as.numeric(alpha_i[a_in==1])*as.numeric(a_in[a_in==1])
         
-        weight_delta<-t(st$l_weight*
-                      t(t(teacher-out_act)%*%as.numeric(alpha_i))*
-                      as.numeric(a_in))[cCat,]
-        
-        ## but adjust only for the correct category
-        w_in_out[cCat,]<-w_in_out[cCat,]+weight_delta
+        ## adjust for the correct category
+        w_in_out[cCat,a_in==1]<-w_in_out[cCat,a_in==1]+weight_delta
         
         ## Gradient of error for exemplar gain change
+        ## gain from current exemplar X to present input node I 
         ## Equation (10)
-        ex_weights_delta<- a_ex%*%(st$l_ex*
-                           (g-gain_inits)*gain_inits)
+        ## note: a_ex in equation 10 is quite redundant as it is always 1
+        ## so I am not sure, whether it has a deeper meaning here...
+        ex_weights_delta<- st$l_ex*(g[a_in==1]-g_inits[a_in==1])*
+                            g_inits[a_in==1]*a_ex[a_ex==1]
         
         
         ## adjusts exemplar-node-to-gain weights for
-        ## every exemplar in memory
-        w_exemplars<-ex_weights_delta+w_exemplars
+        w_exemplars[a_ex==1,a_in==1]<-ex_weights_delta+w_exemplars[a_ex==1,a_in==1]
+        
+        ## adjust bias to category weight
+        ## the formula here is adapted from Kruschke, 1996, ADIT
+        ## where the attention bias is calculated in Equation 10 (p.18)
+        ## there, however, is an additional term called eta,
+        ## which does not seem necessary here?
+        w_bias<-w_bias+(teacher-out_act)*
+            st$beta/(st$beta+st$nFeat^(1/st$eta))
         }
 
         
     probs_out[j,]<-probs
+    if (extendx==T){
+        E4_exemplar_weights[[j]] <-w_exemplars
+        weights_in_out[[j]]<-w_in_out
+        gains[j,]<-g_inits
+        attention[j,]<-alpha_i
+        bias_weight[j,]<-w_bias
     }
     
-    output<-list()
-    output$response_probabilities<-probs_out
-    output$E4_exemplar_weights<-w_exemplars
-    output$E1_w_in_out<-w_in_out
-    output$E4_gains<-g
-    output$E5_attention_strengths<-alpha_i
+    }
+    if (extendx==F){
+        output<-list()
+        output$response_probabilities<-probs_out
+    } else {
+        output<-list()
+        output$response_probabilities<-probs_out
+        output$E4_exemplar_weights<-E4_exemplar_weights
+        output$E1_w_in_out<-weights_in_out
+        output$E4_gains<-gains
+        output$E5_attention_strengths<-attention
+        output$bias_weights<-bias_weight
+    }
+    
     return(output)
 }
 
