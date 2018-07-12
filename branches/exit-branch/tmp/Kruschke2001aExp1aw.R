116238ac4cd9131896260b3e24ac710444109ef0
ajwills <ajwills@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-07-12 16:56:28 +0000

    Clearing exit-branch of everything by files to merge to trunk, plus a tmp directory for unfinished business.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@371 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/exit-branch/tmp/Kruschke2001aExp1aw.R
@@ -0,0 +1,15 @@
+## Inverse Base Rate Effect
+## Replication of Kruschke (2001) simulation of Kruschke (1996, Exp. 1)
+rm(list=ls())
+source("slpEXIT.R")
+source("krus96train.R")
+source("krus96exit.R")
+out <- krus96exit()
+
+## Load Kruschke's sim results (2001) and compare.
+krus.sim <- read.csv("../krus96_sim_krus01.csv", stringsAsFactors = FALSE)
+
+comp <- merge(krus.sim, out)
+comp$diff <- round(comp$kprop- comp$prop, 2)
+print(comp)
+
