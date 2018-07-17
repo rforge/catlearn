5521533ce6d79b8ccf8990e20b5a68017d1fc323
ajwills <ajwills@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-07-17 17:01:21 +0000

    Automated tests are now inside package, so unit_tests directory no longer needed.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/catlearn@384 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- unit_tests/test_act2probrat.R
+++
@@ -1,7 +0,0 @@
-# Unit test for act2probrat
-
-test.act2probrat <- function() {
-    out <- act2probrat(c(0.91,0.54,0.11),3.1,0.24)
-    corr <- c(0.8886475, 0.7170753, 0.4005918)
-    sum((out-corr)^2) < 1e-14  # Return TRUE or FALSE
-}
--- unit_tests/test_slpcovis.R
+++
@@ -1,9 +0,0 @@
-test.slpCOVIS <- function() {
-    load("data/test_slpcovis.RData")
-    set.seed(7)
-    out <- slpCOVIS(st,tr,crx = TRUE,respt = FALSE,rgive = TRUE,
-                    xtdo = FALSE)
-    sum((out$foutmat - testres$foutmat)^2) == 0
-}
-
-
